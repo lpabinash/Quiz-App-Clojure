a0bb05eb0a9bb81aeb640f2ba2834baf20d09425
satyam3918 <46595745+satyam3918@users.noreply.github.com>
2021-05-10 16:20:51 +0530

    Add files via upload

---
+++ app1/project.clj
@@ -0,0 +1,16 @@
+ (defproject app1 "0.1.0-SNAPSHOT"
+   :description "FIXME: write description"
+   :dependencies [[org.clojure/clojure "1.10.0"]
+                  [metosin/compojure-api "2.0.0-alpha30"]
+                  [ring/ring "1.6.3"]
+                  [compojure "1.6.1"]
+                  [manifold "0.1.8"]
+                  [metosin/spec-tools "0.9.2"]
+                  [korma "0.4.3"]
+                  [mysql/mysql-connector-java "8.0.12"]
+                  ]
+   :ring {:handler app1.handler/app}
+   :uberjar-name "server.jar"
+   :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]
+                                  [ring/ring-mock "0.3.2"]]
+                   :plugins [[lein-ring "0.12.5"]]}})
