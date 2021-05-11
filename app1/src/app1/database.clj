42028e94fc0ece3e3db2b223cc9545117b6ec1bb
KishanrajPG <69359891+KishanrajPG@users.noreply.github.com>
2021-05-11 17:51:29 +0530

    Delete app1 directory

--- app1/src/app1/database.clj
+++
@@ -1,9 +0,0 @@
-
-(ns app1.database
-  (:require [korma.db :as korma]))
-
-(def   db-connection-info (korma/mysql                          {:classname "com.mysql.cj.jdbc.Driver"                           :subprotocol "mysql"                           :user ""
-                                                                 :subname "//localhost:3000/exam"}))
-
-; set up korma
-(korma/defdb db db-connection-info)
