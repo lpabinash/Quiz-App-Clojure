56367413d9cc88b6c2e409989926352d917d4f84
amVachan <67003926+amVachan@users.noreply.github.com>
2021-05-12 23:20:22 +0530

    Add files via upload

--- backend/quizapp/src/quizapp/handler.clj
+++ backend/quizapp/src/quizapp/handler.clj
@@ -1,11 +1,17 @@
 (ns quizapp.handler
-  (:require [compojure.api.sweet :refer :all]
+  (:require 
+            [compojure.api.sweet :refer :all]
             [ring.util.http-response :refer :all]
+            [quizapp.middleware :refer [wrap-cors]]
             [schema.core :as s]
-            [quizapp.query :as q]))
+            [quizapp.query :as q]
+            ))
 
 
-(def app
+;; accept everything
+
+
+(def app 
   (api
     {:swagger
      {:ui "/"
@@ -13,16 +19,13 @@
       :data {:info {:title "quizapp"
                     :description "Compojure Api example"}
              :tags [{:name "api", :description "some apis"}]}}}
-    
-    
 
     (context "/quizapp" []
       :tags ["quizapp"]
-
       
-      (GET "/users" []
+     (GET "/users" []
         (ok (q/get-users )))
-      (GET "/users/:UserID" []
+      (GET "/users/UserID" []
         :path-params [UserID :- s/Int]
         (ok (q/get-user UserID)))
       (POST "/add-user" []
@@ -35,6 +38,25 @@
           (ok {:updated (q/update-user UserID descmark)})))
       (DELETE "/users/UserID" []
         :path-params [UserID :- s/Int]
-        (ok {:deleted (q/delete-user UserID)}))
+        (ok {:deleted (q/delete-user UserID)})) 
+
+     
+      
+  
+
+;      (POST "/add-answer" []
+;        :body [answer-body q/AddAnswer]
+;        (let [{:keys [UserID email question answer]} answer-body]
+;          (ok (q/add-answer UserID email question answer))))
+;      (GET "/answers/:UserID" []
+;        :path-params [UserID :- s/Int]
+;        (ok (q/show-answers UserID)))
 
       )))
+
+  (def handler
+  (wrap-cors app :access-control-allow-origin [#"http://localhost:3001"]
+                       :access-control-allow-methods [:get :put :post :delete]))
+   
+      
+
