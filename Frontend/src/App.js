b9099ab96ed8fc937940abe86bf5ba62a834ccb5
KishanrajPG <69359891+KishanrajPG@users.noreply.github.com>
2021-05-20 17:24:25 +0530

    Add files via upload

---
+++ Frontend/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { Router, Switch, Route } from "react-router-dom";
+
+import Desc from "./components/Desc";
+import history from './history';
+import Main from "./Main";
+import Admin from './components/Login/Admin'
+import Login from './components/Login/Login'
+import { User } from "./components/Login/User";
+
+export class App extends Component {
+  state = {
+    loggedIn: false,
+  };
+  render() {
+    return (
+      <div>
+         <Router history={history}>
+                <Switch>
+                    <Route path="/admin" exact component={Admin} />
+                    <Route path="/user" exact component={User} />
+                    <Route path="/Desc" exact component={Desc} />
+                    <Route path="/quiz" exact component={Main} />
+                    <Route path="/"  component={Login} />
+                    
+                </Switch>
+            </Router>
+      </div>
+    )
+  }
+}
+
+export default App
+
